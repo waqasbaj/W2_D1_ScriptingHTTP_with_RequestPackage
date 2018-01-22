e541d165959def0c8714325b152ac4c6a0f1fca7
Waqas Bajwa <waqasbaj@gmail.com>
2018-01-22 17:38:03 +0000

    Good exercise to learn response and to understand how asyncronous coding responds

---
+++ Scripting_HTTP_RequestPackage.js
@@ -0,0 +1,27 @@
+// require `request` and the Node `fs` (filesystem) module
+var request = require('request');
+var fs = require('fs');
+
+request.get('https://sytantris.github.io/http-examples/future.jpg')
+              // Note 1
+       .on('error', function (err) {                                   // Note 2
+         throw err;
+       })
+       .on('response', function (response) {
+        console.log('Downloading image...');                          // Note 3
+         console.log('Response Status Code: ', response.statusCode);
+         console.log('Response Message: ', response.statusMessage);
+         console.log('Content Type: ', response.headers['content-type']);
+
+       })
+       .on('end', function() {
+        console.log('Download complete.');
+        })
+        .pipe(fs.createWriteStream('./future.jpg'))
+              // Note 4
+
+// Notes:
+// 1. `request.get` is equivalent to `request()`
+// 2. `request.on('error', callback)` handles any error
+// 3. `request.on('response, callback)` handles the response
+// 4. What is happening here?
